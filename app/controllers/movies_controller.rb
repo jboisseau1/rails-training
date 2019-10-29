8e95973e5fd5ce98ec90d0a71d8eec2d1f4102db
Jacob Boisseau <jacobboisseau@gmail.com>
2019-10-29 14:03:21 -0400

    training: added partials for header and footer also made form elements

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
     def index
-        @movies = Movie.all
+        @movies = Movie.released
     end
 
     def show
@@ -14,9 +14,25 @@ class MoviesController < ApplicationController
 
     def update
         @movie = Movie.find(params[:id])
-        movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
         @movie.update(movie_params)
 
         redirect_to @movie
     end
+
+    def create
+        @movie = Movie.new(movie_params)
+        @movie.save
+        redirect_to @movie
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_url
+    end
+
+    private
+        def movie_params
+             params.require(:movie).permit(:director, :duration, :image_file_name, :title, :description, :rating, :released_on, :total_gross)
+        end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
     def flop?
         total_gross < 255_000_000
     end
+
+    def self.released
+        where("released_on < ?", Time.now).order(released_on: :desc)
+    end
 end
